98d75d60f21fa40458731af9580965b199880a5e
Almahsol <40520596+ALtabs@users.noreply.github.com>
2024-10-03 19:55:47 +0800

    Add files via upload

---
+++ bulletin_board/articles/views.py
@@ -0,0 +1,14 @@
+from rest_framework import viewsets
+from .models import Article, Comment
+from .serializers import ArticleSerializer, CommentSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
